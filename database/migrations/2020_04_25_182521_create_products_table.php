0937b6d2d5d8b952f9dbe9a665f03c1764f51a34
nadamson <nikolay-256@yandex.ru>
2020-04-25 01:57:40 +0300

    migrate products

--- database/migrations/2020_04_25_182521_create_products_table.php
+++ database/migrations/2020_04_25_182521_create_products_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateDevicesTable extends Migration
+class CreateProductsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,11 @@ class CreateDevicesTable extends Migration
      */
     public function up()
     {
-        Schema::create('devices', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
+            $table->float('price');
+            $table->string('position');
+            $table->text('title');
             $table->timestamps();
         });
     }
@@ -26,6 +29,6 @@ class CreateDevicesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('devices');
+        Schema::dropIfExists('products');
     }
 }
