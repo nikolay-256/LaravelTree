46ecaf59530b65f883a70852b8a7b59325ef8c58
nadamson <n.adamson@jetmail.cc>
2020-06-15 11:03:53 +0300

    tranclate

--- tests/Unit/ItemsOrderTest.php
+++ tests/Unit/ItemsOrderTest.php
@@ -11,7 +11,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 class ItemsOrderTest extends TestCase
 {
     /**
-     * Проверка упорядочивания продкутов по позициям
+     * Checking product ordering by position
      *
      * @return void
      */
@@ -28,7 +28,7 @@ class ItemsOrderTest extends TestCase
         $items[] = (new Item())->setPosition('2.1');
         $items[] = (new Item())->setPosition('3.1.2');
         $items[] = (new Item())->setPosition('3.1.3.6');
-        shuffle($items);//перемешиваем
+        shuffle($items);
         $items = Items::order($items);
         self::assertEquals($items[1]['value']->getPosition(), '1');
         self::assertEquals($items[2]['childs'][3]['value']->getPosition(), '2.3');
